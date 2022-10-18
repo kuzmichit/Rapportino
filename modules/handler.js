d32fb405c04032d6e73460e0b44c7ad3a56f5f29
kuzmichit@gmail.com <kuzmichit@gmail.com>
2022-10-18 13:26:15 +0200

    da inserire validate hour

--- modules/handler.js
+++ modules/handler.js
@@ -2,7 +2,7 @@ import { camellizeClass, isObject, deleteNode, isUserDataInLocalStorage} from '.
 import { CreateCalendar } from './calendar.js';
 import { renderDay, renderModalSignIn } from './renders.js';
 import {onBtnRegisterFormHandler} from './registers_form.js';
-// import { getForm } from './login.js';
+import { initLoginForm } from './login.js';
 
 
 export class MainHandler {
@@ -82,7 +82,7 @@ export class MainHandler {
     else {
       evt.target.style.display = 'none';
       renderModalSignIn();
-      getForm();
+      initLoginForm();
     } 
   }
 }
--- modules/login.js
+++ modules/login.js
@@ -1,8 +1,11 @@
 import { isValid } from "./support.js";
 
+export function initLoginForm() {
+
   const formLogin = document.getElementById('login-form');
   let btnGhost = formLogin['btn-ghost'];
   btnGhost.addEventListener('pointerdown', btnLoginHandler);
+}
 
 function UserData(email, password) {
   this.email = email,
